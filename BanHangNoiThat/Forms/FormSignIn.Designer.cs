00c488d50d925ad768f1bd8d3fd94113ec6c9c22
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:18:19 +0700

    Update BanHangNoiThat/Forms/FormSignIn.Designer.cs

---
+++ BanHangNoiThat/Forms/FormSignIn.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace BanHangNoiThat.Forms
+{
+    partial class FormSignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSignIn));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.header = new System.Windows.Forms.Panel();
+            this.Lablename = new System.Windows.Forms.Label();
+            this.usernameSignIn = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.passwordSignIn = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.Confirmpassword = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.email = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btnConfirm = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.header.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(110)))), ((int)(((byte)(147)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(266, 443);
+            this.panel1.TabIndex = 1;
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.header.Controls.Add(this.Lablename);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(266, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(354, 43);
+            this.header.TabIndex = 2;
+            // 
+            // Lablename
+            // 
+            this.Lablename.AutoSize = true;
+            this.Lablename.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lablename.ForeColor = System.Drawing.Color.White;
+            this.Lablename.Location = new System.Drawing.Point(18, 8);
+            this.Lablename.Name = "Lablename";
+            this.Lablename.Size = new System.Drawing.Size(320, 27);
+            this.Lablename.TabIndex = 3;
+            this.Lablename.Text = "ANH SANG FURNITURE ";
+            // 
+            // usernameSignIn
+            // 
+            this.usernameSignIn.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.usernameSignIn.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.usernameSignIn.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.usernameSignIn.BorderThickness = 3;
+            this.usernameSignIn.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usernameSignIn.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.usernameSignIn.ForeColor = System.Drawing.Color.DimGray;
+            this.usernameSignIn.isPassword = false;
+            this.usernameSignIn.Location = new System.Drawing.Point(273, 111);
+            this.usernameSignIn.Margin = new System.Windows.Forms.Padding(4);
+            this.usernameSignIn.Name = "usernameSignIn";
+            this.usernameSignIn.Size = new System.Drawing.Size(338, 40);
+            this.usernameSignIn.TabIndex = 5;
+            this.usernameSignIn.Text = "User name";
+            this.usernameSignIn.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // passwordSignIn
+            // 
+            this.passwordSignIn.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.passwordSignIn.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.passwordSignIn.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.passwordSignIn.BorderThickness = 3;
+            this.passwordSignIn.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordSignIn.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.passwordSignIn.ForeColor = System.Drawing.Color.DimGray;
+            this.passwordSignIn.isPassword = false;
+            this.passwordSignIn.Location = new System.Drawing.Point(273, 162);
+            this.passwordSignIn.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordSignIn.Name = "passwordSignIn";
+            this.passwordSignIn.Size = new System.Drawing.Size(338, 40);
+            this.passwordSignIn.TabIndex = 6;
+            this.passwordSignIn.Text = "Password";
+            this.passwordSignIn.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.passwordSignIn.OnValueChanged += new System.EventHandler(this.bunifuMetroTextbox1_OnValueChanged);
+            // 
+            // Confirmpassword
+            // 
+            this.Confirmpassword.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Confirmpassword.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Confirmpassword.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Confirmpassword.BorderThickness = 3;
+            this.Confirmpassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Confirmpassword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.Confirmpassword.ForeColor = System.Drawing.Color.DimGray;
+            this.Confirmpassword.isPassword = false;
+            this.Confirmpassword.Location = new System.Drawing.Point(273, 214);
+            this.Confirmpassword.Margin = new System.Windows.Forms.Padding(4);
+            this.Confirmpassword.Name = "Confirmpassword";
+            this.Confirmpassword.Size = new System.Drawing.Size(338, 40);
+            this.Confirmpassword.TabIndex = 7;
+            this.Confirmpassword.Text = "Confirm Password";
+            this.Confirmpassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // email
+            // 
+            this.email.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.email.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.email.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.email.BorderThickness = 3;
+            this.email.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.email.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.email.ForeColor = System.Drawing.Color.DimGray;
+            this.email.isPassword = false;
+            this.email.Location = new System.Drawing.Point(272, 264);
+            this.email.Margin = new System.Windows.Forms.Padding(4);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(338, 40);
+            this.email.TabIndex = 8;
+            this.email.Text = "Email";
+            this.email.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Location = new System.Drawing.Point(268, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(350, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Anh Sang Furniture - Enhance the value of your home";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(386, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Sign In";
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.header;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // btnexit
+            // 
+            this.btnexit.FlatAppearance.BorderSize = 0;
+            this.btnexit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnexit.Image = global::BanHangNoiThat.Properties.Resources.gnome_session_hibernate1;
+            this.btnexit.Location = new System.Drawing.Point(589, 45);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(31, 30);
+            this.btnexit.TabIndex = 12;
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.ActiveBorderThickness = 1;
+            this.btnConfirm.ActiveCornerRadius = 20;
+            this.btnConfirm.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnConfirm.ActiveForecolor = System.Drawing.Color.White;
+            this.btnConfirm.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnConfirm.BackColor = System.Drawing.SystemColors.Control;
+            this.btnConfirm.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnConfirm.BackgroundImage")));
+            this.btnConfirm.ButtonText = "Confirm";
+            this.btnConfirm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirm.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnConfirm.IdleBorderThickness = 1;
+            this.btnConfirm.IdleCornerRadius = 20;
+            this.btnConfirm.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnConfirm.IdleForecolor = System.Drawing.Color.White;
+            this.btnConfirm.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnConfirm.Location = new System.Drawing.Point(272, 331);
+            this.btnConfirm.Margin = new System.Windows.Forms.Padding(5);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(338, 51);
+            this.btnConfirm.TabIndex = 9;
+            this.btnConfirm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnConfirm.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // FormSignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 443);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.Confirmpassword);
+            this.Controls.Add(this.passwordSignIn);
+            this.Controls.Add(this.usernameSignIn);
+            this.Controls.Add(this.header);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormSignIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormSignIn";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.header.ResumeLayout(false);
+            this.header.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel header;
+        private Bunifu.Framework.UI.BunifuMetroTextbox usernameSignIn;
+        private Bunifu.Framework.UI.BunifuMetroTextbox passwordSignIn;
+        private Bunifu.Framework.UI.BunifuMetroTextbox Confirmpassword;
+        private Bunifu.Framework.UI.BunifuMetroTextbox email;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnConfirm;
+        private System.Windows.Forms.Label Lablename;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnexit;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+    }
+}
