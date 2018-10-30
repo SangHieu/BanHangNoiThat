fca8e7d8d223a93f32463107ccd3d9acc4f6fc39
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:18:17 +0700

    Update BanHangNoiThat/Forms/FormSignIn.cs

---
+++ BanHangNoiThat/Forms/FormSignIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanHangNoiThat.Forms
+{
+    public partial class FormSignIn : Form
+    {
+        public FormSignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            GiaodienUser fr = new GiaodienUser();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void bunifuMetroTextbox1_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
