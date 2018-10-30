14c7d712492896ec65395d2d986dfc27b78b28ec
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:18:23 +0700

    Update BanHangNoiThat/Forms/GiaodienAdim.cs

---
+++ BanHangNoiThat/Forms/GiaodienAdim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanHangNoiThat.Forms
+{
+    public partial class GiaodienAdim : Form
+    {
+        public GiaodienAdim()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQLKH_Click(object sender, EventArgs e)
+        {
+            aminQLKH1.BringToFront();
+        }
+
+        private void btnQLSP_Click(object sender, EventArgs e)
+        {
+            aminQLSP1.BringToFront();
+        }
+    }
+}
