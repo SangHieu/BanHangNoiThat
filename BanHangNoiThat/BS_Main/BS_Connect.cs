ed97e90af606452e8b43ab82002881ffdef4a638
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:47:46 +0700

    Update BanHangNoiThat/BS_Main/BS_Connect.cs

---
+++ BanHangNoiThat/BS_Main/BS_Connect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BanHangNoiThat.DB_main;
+
+namespace BanHangNoiThat.BS_Main
+{
+    public class BS_Connect
+    {
+        DBmain db = null;
+        public BS_Connect()
+        {
+            db = new DBmain();
+        }
+    }
+}
