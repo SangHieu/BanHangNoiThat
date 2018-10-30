beba7640b83372b379814a887c28388e836561f2
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:18:37 +0700

    Update BanHangNoiThat/Forms/UsersControl/AddCart.cs

---
+++ BanHangNoiThat/Forms/UsersControl/AddCart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanHangNoiThat.Forms.UsersControl
+{
+    public partial class AddCart : UserControl
+    {
+        public AddCart()
+        {
+            InitializeComponent();
+        }
+
+        private void Motasp_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
