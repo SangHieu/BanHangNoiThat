8714cf1c1172f383d2bbfef7d191bbc02b8b0662
Anh Sang Nguyễn <44453729+konkunkut@users.noreply.github.com>
2018-10-30 15:19:29 +0700

    Update BanHangNoiThat/Forms/UsersControl/UserHome.cs

---
+++ BanHangNoiThat/Forms/UsersControl/UserHome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace BanHangNoiThat.Forms.UsersControl
+{
+    public partial class UserHome : UserControl
+    {
+        public UserHome()
+        {
+            InitializeComponent();
+        }
+
+        private void UserHome_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
